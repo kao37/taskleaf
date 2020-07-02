e6fc4066c9fdc41cc7f128bb4031e84f460c9b0d
kao37 <4107937@gmail.com>
2020-07-02 14:00:23 +0900

    add-確認画面実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,9 +15,19 @@ class TasksController < ApplicationController
   def edit
   end
 
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
+  end
+
   def create
     @task = current_user.tasks.new(task_params)
-    
+
+    if params[:back].present?
+      render :new
+      return
+    end
+
     if @task.save
       redirect_to  @task, notice: "タスク「#{@task.name}」を登録しました。"
     else 
